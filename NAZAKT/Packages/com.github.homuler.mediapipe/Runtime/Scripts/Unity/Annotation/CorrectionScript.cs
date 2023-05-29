48275738738754abde0f8939574a7fca1fe22f26
socra167@gmail.com <socra167@gmail.com>
2023-05-29 17:37:55 +0900

    feat: 무릎위치 및 무게중심 판별 로직 추가, CalcAngle() 확장

--- NAZAKT/Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/CorrectionScript.cs
+++ NAZAKT/Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/CorrectionScript.cs
@@ -16,15 +16,15 @@ namespace Mediapipe.Unity
         private WOInfo info;
         private ConnectionListAnnotation connectionList1;
         private ConnectionListAnnotation connectionList2;
-        private int currentState; // ?????? ?? ???? ????(1, 2, 3)
-        private bool isSquattingDown; // ????? ????? ???????? ??? ??????, State3?? ???? ?? False, State1?? ???? ?? True?? ????
+        private int currentState; // 1, 2, 3
+        private bool isSquattingDown;
         private bool isDeepSquat; // Deep Squats Flag
 
         // Start is called before the first frame update
         void Start()
         {
             time = 1f;
-            currentState = 1; // ?? ???? ???? ????
+            currentState = 1;
             isSquattingDown = true;
             isDeepSquat = false;
             connectionList1 = connectionListAnnotation1.GetComponent<ConnectionListAnnotation>();
@@ -49,8 +49,8 @@ namespace Mediapipe.Unity
             return vector;
         }
 
-        // Calculate angle with Landmark datas
-        // num3 = -1 ?? ???? y??? ?????? ?????
+        // Calculate angle with Landmark datas (num1 - num2 - num3)
+        // when num3 = -1, calculate angle with y axis
         float CalcAngle(int num1, int num2, int num3){
             Vector3 VA, VB;
             if (num3 == -1){
@@ -64,7 +64,35 @@ namespace Mediapipe.Unity
             return angle;
         }
 
-        // num ??�� Connection?? ???? ?????????? ??????
+        // Calculate angle of (num1 - middle of num2, num3 - num4)
+        float CalcAngle(int num1, int num2, int num3, int num4){
+            Vector3 VA, VB, Vcenter;
+            Vcenter = GetVector(num2) + GetVector(num3);
+            Vcenter /= 2;
+            VA = GetVector(num1) - Vcenter;
+            VB = GetVector(num4) - Vcenter;
+            float angle = Vector3.Angle(VA, VB);
+            return angle;
+        }
+
+        // Calculate angle of (middle of num1, num2 - middle of num3, num4 - y)
+        // when num5 = -1, calculate angle with y axis (middle of num1, num2 - middle of num3, num4 - y)
+        float CalcAngle(int num1, int num2, int num3, int num4, int num5){
+            Vector3 VA, VB;
+            if (num5 == -1){
+                VA = ((GetVector(num1) + GetVector(num2)) - (GetVector(num3) + GetVector(num4))) / 2;
+                VB = new Vector3(0, -0.1f, 0);
+            } else {
+                // temporary
+                VA = GetVector(num1) - GetVector(num2);
+                VB = GetVector(num3) - GetVector(num2);
+            }
+            float angle = Vector3.Angle(VA, VB);
+            Debug.Log(angle);
+            return angle;
+        }
+
+        // change color to Red at numth connection
         void ChangeToRed(int num){
             if (!this.connectionList1.wrongNumbers.Contains(num)){
                 this.connectionList1.wrongNumbers.Add(num);
@@ -72,18 +100,12 @@ namespace Mediapipe.Unity
             }
         }
 
-        // num ??�� Connection?? ???? ??????? ??????
+        // change color to White at numth connection
         void ChangeToWhite(int num){
             if (this.connectionList1.wrongNumbers.Contains(num)){
                 this.connectionList1.wrongNumbers.Remove(num);
                 this.connectionList2.wrongNumbers.Remove(num);
             }
-        }
-
-        // 상체가 앞으로 쏠렸는가 판단 함수
-        bool isUpperBodyLeaningForward(int left_angle, int right_angle){
-            
-            return true;
         }
 
         void CheckPose(){
@@ -92,15 +114,15 @@ namespace Mediapipe.Unity
                 /*
                 States Overview
                 (0 ~ 32)
-                State s1:   If the angle between the knee and the vertical falls within 32??,
+                State s1:   If the angle between the knee and the vertical falls within 32,
                             then it is in the Normal phase, and its state is s1. It is essentially the state
                             where the counters for proper and improper squats are updated.
                 (35 ~ 65)
-                State s2:   If the angle between the knee and the vertical falls between 35?? and 65??,
+                State s2:   If the angle between the knee and the vertical falls between 35 and 65,
                             it is in the Transition phase and subsequently goes to state s2.
                 (75 ~ 95)
                 State s3:   If the angle between the knee and the vertical lies within a specific range
-                            (say, between 75?? and 95??), it is in the Pass phase and subsequently goes to state s3.
+                            (say, between 75 and 95), it is in the Pass phase and subsequently goes to state s3.
                 */
                 
                 /*
@@ -112,7 +134,8 @@ namespace Mediapipe.Unity
                 */
 
                 float left_knee_angle, right_knee_angle;
-                float left_upper_body_angle, right_upper_body_angle; // left(12, 24), right(11, 23)
+                float feet_groin_angle, knees_groin_angle;
+                float shoulder_feet_angle;
 
                 switch (currentState){
                     case 1:
@@ -120,8 +143,23 @@ namespace Mediapipe.Unity
                         right_knee_angle = CalcAngle(24, 26, -1); // Calculate right_knee_angle (vertical)
                         info.currentState = 1;
 
+                        feet_groin_angle = CalcAngle(27, 23, 24, 28);
+                        knees_groin_angle = CalcAngle(25, 23, 24, 26);
+
+                        shoulder_feet_angle = CalcAngle(11, 12, 27, 28, -1);
+
+                        // Check knee position
+                        if (knees_groin_angle < feet_groin_angle){
+                            Debug.Log("Knees position Wrong!!!");
+                        }
+
+                        // Check shoulder feet angle
+                        if (15 < shoulder_feet_angle){
+                            // Debug.Log("Shoulder - Feet angle Wrong!!!");
+                        }
+
                         // State1 -> State2
-                        if (!isSquattingDown){ // State3???? ?????? ?? ??? case?? ?????? ???
+                        if (!isSquattingDown){ // If reached the case again after going down to State3
                             if (isDeepSquat){
                                 // Incorrect Count up
                                 info.increseIncorrectCount();
@@ -142,9 +180,24 @@ namespace Mediapipe.Unity
                     case 2:
                         left_knee_angle = CalcAngle(23, 25, -1); // Calculate left_knee_angle (vertical)
                         right_knee_angle = CalcAngle(24, 26, -1); // Calculate right_knee_angle (vertical)
+
+                        feet_groin_angle = CalcAngle(27, 23, 24, 28);
+                        knees_groin_angle = CalcAngle(25, 23, 24, 26);
+
+                        shoulder_feet_angle = CalcAngle(11, 12, 27, 28, -1);
                         
+                        // Check knee position
+                        if (knees_groin_angle < feet_groin_angle){
+                            Debug.Log("Knees position Wrong!!!");
+                        }
+
+                        // Check shoulder feet angle
+                        if (15 < shoulder_feet_angle){
+                            // Debug.Log("Shoulder - Feet angle Wrong!!!");
+                        }
+
                         // State2 -> State1
-                        if ((left_knee_angle < 32) && (right_knee_angle < 32) && (!isSquattingDown)){ // State1?? ????
+                        if ((left_knee_angle < 32) && (right_knee_angle < 32) && (!isSquattingDown)){ // Transition to State1
                             currentState = 1;
                             info.SetState(currentState);
                         } else if ((left_knee_angle < 32) && (right_knee_angle < 32) && (isSquattingDown)){
@@ -160,7 +213,7 @@ namespace Mediapipe.Unity
                             info.SetState(currentState);
                             isSquattingDown = false;
                         } else if ((75 < left_knee_angle && left_knee_angle < 95) && (75 < right_knee_angle && right_knee_angle < 95) && (!isSquattingDown)){
-                            // ???? ???? ?????? ??�� ???? ???
+                            // If you go down when you should go up
                             currentState = 3;
                             info.SetState(currentState);
                         }
@@ -169,6 +222,21 @@ namespace Mediapipe.Unity
                     case 3:
                         left_knee_angle = CalcAngle(23, 25, -1); // Calculate left_knee_angle (vertical)
                         right_knee_angle = CalcAngle(24, 26, -1); // Calculate right_knee_angle (vertical)
+
+                        feet_groin_angle = CalcAngle(27, 23, 24, 28);
+                        knees_groin_angle = CalcAngle(25, 23, 24, 26);
+                        
+                        shoulder_feet_angle = CalcAngle(11, 12, 27, 28, -1);
+
+                        // Check knee position
+                        if (knees_groin_angle < feet_groin_angle){
+                            Debug.Log("Knees position Wrong!!!");
+                        }
+
+                        // Check shoulder feet angle
+                        if (15 < shoulder_feet_angle){
+                            // Debug.Log("Shoulder - Feet angle Wrong!!!");
+                        }
 
                         // Too deep Squat
                         if ((95 < left_knee_angle) || (95 < right_knee_angle)){
