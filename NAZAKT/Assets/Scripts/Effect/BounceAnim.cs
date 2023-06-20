2eb75aa53134e00fd98ca5b58dac71784097059b
dannyjoo7 <danny_joo@naver.com>
2023-06-20 15:28:28 +0900

    feat: 설명창 추가

---
+++ NAZAKT/Assets/Scripts/Effect/BounceAnim.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceAnim : MonoBehaviour
+{
+    float time = 0;
+    public float _size = 5;
+
+    public float _upSizeTime =0.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.activeSelf){
+            if(time <= _upSizeTime)
+                {
+                    transform.localScale = Vector3.one * (1 + _size * time);
+                }
+                else if (time <= _upSizeTime*2)
+                {
+                    transform.localScale = Vector3.one * (2*_size * _upSizeTime + 1 - time * _size);
+                }
+                else
+                {
+                    transform.localScale = Vector3.one;
+                }
+                time += Time.deltaTime;
+        } else resetAnim();
+    }
+
+    public void resetAnim()
+    {
+        time = 0;
+    }
+
+}
---
+++ NAZAKT/Assets/Scripts/nazakt/ExerciseExplainer.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class ExerciseExplainer : MonoBehaviour
+{
+    public GameObject img;
+    public GameObject txt;
+    public GameObject WOName;
+    public GameObject startBtn;
+    Sprite[] sprites;
+    string[] explain;
+    int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        index = 0;
+        sprites = Resources.LoadAll<Sprite>("Img/WOImg");
+        initExplain();
+        img.GetComponent<Image>().sprite = sprites[index];
+        txt.GetComponent<TextMeshProUGUI>().text = explain[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetExerciseExplainer(){
+        index = 0;
+        startBtn.SetActive(false);
+        img.GetComponent<Image>().sprite = sprites[index];
+        txt.GetComponent<TextMeshProUGUI>().text = explain[index];
+    }
+
+    void initExplain(){
+        // 운동 이름 초기화
+        transform.Find("Explaintext").GetComponent<TextMeshProUGUI>().text =  WOName.GetComponent<TextMeshProUGUI>().text + " 설명";
+       
+        // 운동 설명 초기화
+        explain = new string[sprites.Length];
+        for(int i = 0; i < sprites.Length; i++){
+            if(i == sprites.Length - 1){
+                explain[i] = "마지막 페이지" ;
+                break;
+            }
+            explain[i] = (i + 1) + "페이지" ;
+        }
+    }
+
+    // 오른쪽 버튼 눌렀을 시
+    public void FlipRightBtn(){
+        if(index >= sprites.Length - 1){
+            return;
+        }
+        else {
+            // 이미지 설정
+            img.GetComponent<Image>().sprite = sprites[++index];
+
+            // 설명 설정
+            txt.GetComponent<TextMeshProUGUI>().text = explain[index];
+
+            // 마지막 페이지
+            if(index >= sprites.Length - 1){
+                startBtn.SetActive(true);
+            }
+        }
+    }
+    
+    // 왼쪽 버튼 눌렀을 시
+    public void FlipLeftBtn(){
+        if(index <= 0){
+            return;
+        }
+        else {
+            // 이미지 설정
+            img.GetComponent<Image>().sprite = sprites[--index];
+
+            // 설명 설정
+            txt.GetComponent<TextMeshProUGUI>().text = explain[index];
+
+            // 마지막 페이지
+            if(index <= sprites.Length - 1){
+                startBtn.GetComponent<BounceAnim>().resetAnim();
+                startBtn.SetActive(false);
+            }
+        }
+    }
+}
